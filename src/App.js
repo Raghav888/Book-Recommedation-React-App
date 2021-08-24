d1e4f53a632d55f245cf453645f6871e552c496a
RAGHAV BANG <RAGHAVBANG2012@GMAIL.COM>
2021-08-24 19:10:40 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,84 @@
+import "./styles.css";
+import { useState } from "react";
+
+var name = ["JavaScript", "Fiction", "Finance"];
+var bookdictionary = {
+  JavaScript: [
+    {
+      Title: "Eloquent JavaScript",
+      Rating: "3.5/5"
+    },
+    {
+      Title: "You Don't Know JS",
+      Rating: " 5/5"
+    }
+  ],
+  Fiction: [
+    {
+      Title: "Shiva Trilogy",
+      Rating: "5/5"
+    },
+    {
+      Title: "Harry Potter and the Sorcerer's Stone",
+      Rating: " 2/5"
+    }
+  ],
+  Finance: [
+    {
+      Title: "Rich Dad Poor Dad",
+      Rating: "5/5"
+    },
+    {
+      Title: "Loonshots",
+      Rating: " 2/5"
+    }
+  ]
+};
+
+export default function App() {
+  var [bookselected, setbook] = useState("");
+  function nameBookHandler(name) {
+    bookselected = name;
+    setbook(name);
+  }
+
+  return (
+    <div className="App">
+      <h1>📚 goodbooks</h1>
+      <span>Checkout my favorite books. Select a genre to get started</span>
+      <div id="outer">
+        <div className="inner">
+          {name.map((name) => {
+            console.log(name);
+            return (
+              <button className="msgBtn" onClick={() => nameBookHandler(name)}>
+                {name}
+              </button>
+            );
+          })}
+        </div>
+      </div>
+
+      <hr />
+      {bookselected in bookdictionary ? (
+        <div className="suggestions-box">
+          {bookdictionary[bookselected].map((book) => {
+            return (
+              <div className="output-data">
+                <h3>
+                  <span>Title: </span>
+                  {book.Title}
+                </h3>
+                <p>
+                  <span>Ratings: </span> {book.Rating}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
+}
